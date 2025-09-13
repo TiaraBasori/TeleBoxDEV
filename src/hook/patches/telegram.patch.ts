753eb0fa069eb6e73c2510a19241f7c4a0068cea
xream <xream@icloud.com>
2025-09-13 19:58:55 +0800

    feat: 消息延后删除 deleteWithDelay 默认不报错

--- src/hook/patches/telegram.patch.ts
+++ src/hook/patches/telegram.patch.ts
@@ -1,7 +1,17 @@
 import { Api } from "telegram/tl";
 import { sleep } from "telegram/Helpers";
 
-Api.Message.prototype.deleteWithDelay = async function (delay:number) {
-    await sleep(delay);
+Api.Message.prototype.deleteWithDelay = async function (
+  delay: number,
+  shouldThrowError: boolean
+) {
+  await sleep(delay);
+  try {
     return this.delete();
-}
+  } catch (e) {
+    console.error(e);
+    if (shouldThrowError) {
+      throw e;
+    }
+  }
+};
--- src/hook/types/telegram.d.ts
+++ src/hook/types/telegram.d.ts
@@ -10,12 +10,16 @@ declare module "telegram" {
        * 删除消息，但会先等待指定的毫秒数。
        *
        * @param delay 等待的时间（毫秒）
+       * @param shouldThrowError 是否在删除失败时抛出错误，默认为 false
        * @example
        * ```ts
        * await msg.deleteWithDelay(2000);
        * ```
        */
-      deleteWithDelay(delay: number): Promise<Api.messages.AffectedMessages[] | undefined>;
+      deleteWithDelay(
+        delay: number,
+        shouldThrowError?: boolean
+      ): Promise<Api.messages.AffectedMessages[] | undefined>;
     }
   }
 }
